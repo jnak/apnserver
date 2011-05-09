ea8c9b19a1c6a501d93742da7fdecaa1183c0c3f
jnak <julien.nakache@gmail.com>
2011-05-09 10:10:36 -0700

    Edited lib/apnserver/protocol.rb via GitHub

--- lib/apnserver/protocol.rb
+++ lib/apnserver/protocol.rb
@@ -2,20 +2,20 @@ module ApnServer
   module Protocol
     def post_init
       @address = Socket.unpack_sockaddr_in(self.get_peername)
-      Config.logger.debug "#{Time.now} [#{address.last}:#{address.first}] CONNECT"
+      @loggr.debug "#{Time.now} [#{address.last}:#{address.first}] CONNECT"
     end
 
     def unbind
-      Config.logger.debug "#{Time.now} [#{address.last}:#{address.first}] DISCONNECT"
+      @loggr.debug "#{Time.now} [#{address.last}:#{address.first}] DISCONNECT"
     end
 
     def receive_data(data)
       (@buf ||= "") << data
       if notification = ApnServer::Notification.valid?(@buf)
-        Config.logger.debug "#{Time.now} [#{address.last}:#{address.first}] found valid Notification: #{notification}"
+        @loggr.debug "#{Time.now} [#{address.last}:#{address.first}] found valid Notification: #{notification}"
         queue.push(notification)
       else
-        Config.logger.debug "#{Time.now} [#{address.last}:#{address.first}] invalid notification: #{@buf}"
+        @loggr.debug "#{Time.now} [#{address.last}:#{address.first}] invalid notification: #{@buf}"
       end
     end
   end
